fb29eb5102f3b1cf96b8afed75c778342acd01ee
prithviulag <prithviulaganathan@gmail.com>
2022-08-15 16:54:07 -0400

    added functional interface annotation

--- src/main/java/com/hxmalar/resrdauth/IDatabaseBridge.java
+++ src/main/java/com/hxmalar/resrdauth/IDatabaseBridge.java
@@ -3,6 +3,7 @@ package com.hxmalar.resrdauth;
 import javax.ws.rs.core.Response;
 
 //Functional interface allowing use of AccessProcessor.gatewayAccess for GET, POST, DELETE, PUT, etc.
+@FunctionalInterface
 public interface IDatabaseBridge {
     Response accessDatabase(String input);
 }
